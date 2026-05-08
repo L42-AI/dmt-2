27911975f2e0888b822f212438e091f2bdf6066c
L42-AI <lzaitch@gmail.com>
2026-05-08 21:53:44 +0200

    improve data loading pipeline

--- src/kaggle_interaction/load.py
+++ src/kaggle_interaction/load.py
@@ -3,11 +3,28 @@ from pathlib import Path
 import pandas as pd
 import kagglehub
 
-def load_data() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    """ Returns submission_sample, training_set, test_set dataframes from the competition dataset. """
+def load_submission_sample(path: str | None = None) -> pd.DataFrame:
+    """ Returns the submission sample dataframe from the competition dataset. """
+    if path is None:
+        path = Path(kagglehub.competition_download('dmt-2026-2nd-assignment'))
+    return pd.read_csv(path / 'submission_sample.csv')
+
+def load_training_set(path: str | None = None) -> pd.DataFrame:
+    """ Returns the training set dataframe from the competition dataset. """
+    if path is None:
+        path = Path(kagglehub.competition_download('dmt-2026-2nd-assignment'))
+    return pd.read_csv(path / 'training_set_VU_DM.csv')
+
+def load_test_set(path: str | None = None) -> pd.DataFrame:
+    """ Returns the test set dataframe from the competition dataset. """
+    if path is None:
+        path = Path(kagglehub.competition_download('dmt-2026-2nd-assignment'))
+    return pd.read_csv(path / 'test_set_VU_DM.csv')
+
+def load_data() -> tuple[pd.DataFrame, pd.DataFrame]:
+    """ Returns training_set, test_set dataframes from the competition dataset. """
     path = Path(kagglehub.competition_download('dmt-2026-2nd-assignment'))
     return (
-        pd.read_csv(path / 'submission_sample.csv'),
-        pd.read_csv(path / 'training_set_VU_DM.csv'),
-        pd.read_csv(path / 'test_set_VU_DM.csv')
+        load_training_set(path),
+        load_test_set(path)
     )
