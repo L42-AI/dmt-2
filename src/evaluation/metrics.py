7e376f10544d744348b8be74297f2dc3ede1b1ef
coopa33 <coopa33@coopa33s-macbook-air-1.home>
2026-05-09 18:12:48 +0200

    added ndcg

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -10,7 +10,7 @@ def dcg(relevance_scores: list, top_ranks: int = 5, alt: bool = False) -> float:
                                     relevant documents. Defaults to False.
 
     Returns:
-        float: DCG score for the query
+        float: DCG score for search query results
     """
     top_k = relevance_scores[:top_ranks]
     ranks = range(1, top_ranks + 1) # [1, k]
@@ -19,6 +19,20 @@ def dcg(relevance_scores: list, top_ranks: int = 5, alt: bool = False) -> float:
     else:
         penalized_top_k = [top_k[i - 1] / math.log2(i + 1) for i in ranks]
     return sum(penalized_top_k)
+
+def ndcg_5(relevance_scores: list, alt: bool = False) -> float:
+    """ Normalized discounted cumulative gain for a ranked search query
+
+    Args:
+        relevance_scores (list): The relevance scores of the ranked search query
+        alt (bool, optional): Alternative DCG formulation. Defaults to False.
+
+    Returns:
+        float: NDCG value for search query results.
+    """
+    dcg_5 = dcg(relevance_scores, 5, alt)
+    idcg_5 = dcg(relevance_scores.sort(reverse=True), 5, alt)
+    return dcg_5 / idcg_5
 
 def evaluate(model, data):
     # Dummy implementation for evaluation
