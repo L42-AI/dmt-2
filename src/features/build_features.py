4add126fca1424a3a167c03b1dc75b304e444f81
coopa33 <coopa33@coopa33s-macbook-air-1.home>
2026-05-09 19:52:12 +0200

    added preliminary relevance score builder

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,3 +1,20 @@
+import pandas as pd
+
+def build_relevance_scores(df: pd.DataFrame) -> None:
+    ''' Build the relevance score attribute in the dataframe, according to the values specified in 
+        the assignment document
+
+    Args:
+        df (pd.DataFrame): The dataframe to which to add the relevance score attribute
+    '''
+    which_clicked = df.query('click_bool == 1').index
+    which_booked = df.query('booking_bool == 1').index
+    which_neither = df.query('click_bool == 0 & booking_bool == 0')
+    df.loc[which_clicked, 'rel_score'] = 1
+    df.loc[which_booked, 'rel_score'] = 5
+    df.loc[which_neither, 'rel_score'] = 0
+
+
 def process_features(data, config):
     # Dummy implementation for feature engineering and imputation
     return data
