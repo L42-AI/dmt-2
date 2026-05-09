5895a52c91f9e7128cdd8a2e37a79a1aa09ea040
coopa33 <coopa33@coopa33s-macbook-air-1.home>
2026-05-09 17:53:07 +0200

    added discounted cumulative gain

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -1,3 +1,25 @@
+import math
+
+def dcg(relevance_scores: list, top_ranks: int = 5, alt: bool = False) -> float:
+    """ Discounted cumulative gain score for a ranked list of results in a search query.
+
+    Args:
+        relevance_scores (list):    Relevance scores of ranked properties in a query. 
+        top_ranks (int, optional):  The top-k ranks to look for relevance scores. Defaults to 5.
+        alt (bool, optional):       Alternative DCG formulation, with stronger empathis on retrieving
+                                    relevant documents. Defaults to False.
+
+    Returns:
+        float: DCG score for the query
+    """
+    top_k = relevance_scores[:top_ranks]
+    ranks = range(1, top_ranks + 1) # [1, k]
+    if alt:
+        penalized_top_k = [2 ** top_k[i-1] / math.log2(i + 1) for i in ranks]
+    else:
+        penalized_top_k = [top_k[i - 1] / math.log2(i + 1) for i in ranks]
+    return sum(penalized_top_k)
+
 def evaluate(model, data):
     # Dummy implementation for evaluation
     return {"rmse": 0.0}
