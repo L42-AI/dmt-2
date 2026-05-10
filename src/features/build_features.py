e6b8832b50303a88f9edbcc1b8fcc63df46c8e6a
coopa33 <coopa33@coopa33s-macbook-air-1.home>
2026-05-10 04:45:07 +0200

    continue eda, added binary seasonality and checking feature builders

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
+def build_binary_season(name: str, df: pd.DataFrame, start: int, end: int) -> pd.DataFrame:
+    df[name] = df['date_time'].dt.month.between(start, end)
+    return df
+
+def build_checkin_dates(df: pd.DataFrame) -> pd.DataFrame:
+    df['checkin_date'] = df['date_time'] + pd.to_timedelta(df['srch_booking_window'], unit='D')
+    return df
+
 def build_relevance_scores(df: pd.DataFrame) -> pd.DataFrame:
     ''' Build the relevance score attribute in the dataframe, according to the values specified in 
         the assignment document
@@ -15,7 +23,6 @@ def build_relevance_scores(df: pd.DataFrame) -> pd.DataFrame:
     df.loc[which_neither, 'rel_score'] = 0
     
     return df
-
 
 def process_features(data, config):
     # Dummy implementation for feature engineering and imputation
