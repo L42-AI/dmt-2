1eb22285fcc5fa50df2f3b4eebacfed0267998ac
coopa33 <coopa33@coopa33s-macbook-air-2.home>
2026-05-12 23:17:09 +0200

    fixed imports, added pipeline output prints

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ from pipeline import Pipeline
 
 pipeline = Pipeline()
 
-for approach in ['ceiling', 'baseline', 'advanced', 'xgboost']:
+for approach in ['ceiling', 'baseline', 'lambdamart', 'xgboost']:
     train_predictions, val_predictions, test_predictions, train_acc, val_acc, test_acc = pipeline.run(approach)
+    print(f'=== {approach} ===')
     print(f"Train Accuracy: {train_acc}")
     print(f"Validation Accuracy: {val_acc}")
     print(f"Test Accuracy: {test_acc}")
+    print('\n')
