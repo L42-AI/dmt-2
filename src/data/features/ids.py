b53858733a1302d981397d3b98ac28cf8905fe97
L42-AI <lzaitch@gmail.com>
2026-05-15 19:36:08 +0200

    fix id matching

--- src/data/features/ids.py
+++ src/data/features/ids.py
@@ -7,8 +7,15 @@ def resample_ids(df: pd.DataFrame):
     - 'visitor_location_country_id'
     - 'prop_country_id'
     """
-    for col in ['srch_destination_id', 'visitor_location_country_id', 'prop_country_id']:
-        unique_values = df[col].unique()
-        mapping = {old_id: new_id + 1 for new_id, old_id in enumerate(unique_values)}
-        df[col] = df[col].map(mapping).astype('int32')
+
+    # Resample 'srch_destination_id'
+    unique_destinations = df['srch_destination_id'].drop_duplicates()
+    destination_mapping = {old_id: new_id for new_id, old_id in enumerate(unique_destinations)}
+    df['srch_destination_id'] = df['srch_destination_id'].map(destination_mapping)
+
+    # Resample 'visitor_location_country_id' and 'prop_country_id' together
+    unique_countries = pd.concat([df['visitor_location_country_id'], df['prop_country_id']]).drop_duplicates()
+    country_mapping = {old_id: new_id for new_id, old_id in enumerate(unique_countries)}
+    df['visitor_location_country_id'] = df['visitor_location_country_id'].map(country_mapping)
+    df['prop_country_id'] = df['prop_country_id'].map(country_mapping)
     return df
